2abc1b527aa14c87f5792c5df639c3c6c9a1cc68
Brendan Horan <brendanhoran@basstech.net>
2013-10-08 21:37:53 +0800

    Update ir-blower.ino

--- ir-blower.ino
+++ ir-blower.ino
@@ -2,6 +2,9 @@
 // Licence : BSD 3-Clause
 // Arduino-IRremote Licence : GNU LESSER GENERAL PUBLIC LICENCE Version 2.1
 // Version : 0.0.1
+//
+// DOCUMENT NEC hex values bettter!
+
 
 #include <IRremote.h>
 IRsend irsend;
@@ -29,33 +32,57 @@ void loop() {
                 switch (ctlmsg) {
                   case 49:
                     Serial.print("49");
-                  //  irsend.sendNEC(0x80???,32)
+                  // 0x80,0x84 = master8 select key
+                  //  irsend.sendNEC(0x807f847b,32)
                   //   delay(100);
                     break;
                   case 50:
                     Serial.print("50");
-                 //   irsend.sendNEC(0x80???,32)
+                 // 0x80,0x88 = master8 mute
+                 //   irsend.sendNEC(0x807f8877,32)
                  //   delay(100);
                     break;
                   case 51:
                     Serial.print("51");
-                 //   irsend.sendNEC(0x80???,32)
+                 // 0x80,8c = master8 vol+
+                 //   irsend.sendNEC(0x807f8c73,32)
                  //   delay(100);
                     break;
                   case 52:
                     Serial.print("52");
-                 //   irsend.sendNEC(0x48???,32)
+                 // 0x80,0x90 = master8 vol_
+                 //   irsend.sendNEC(0x807f906f,32)
                  //   delay(100);
                     break;
                   case 53:
                     Serial.print("53");
-                 //   irsend.sendNEC(0x48???,32)
+                 // 0x48,0x0d = NFB17 select1
+                 //   irsend.sendNEC(0x48b709f6,32)
                  //   delay(100);
                     break;
                   case 54:
                     Serial.print("54");
-                 //   irsend.sendNEC(0x48???,32)
+                 // 0x48,0x0d = NFB17 select2
+                 //   irsend.sendNEC(0x48b70df2,32)
                  //   delay(100);
+                    break;
+                  case 55:
+                    Serial.print("55");
+                 // 0x48,0x1a = NFB17 select3
+                 // irsend.sendNEC(0x48b71ae5,32)
+                    delay(100);
+                    break;
+                  case 56:
+                    Serial.print("56");
+                 // 0x48,0x01 = NFB17 select4
+                 // irsend.sendNEC(0x48b701fe,32)
+                    delay(100);
+                    break;
+                  case 57:
+                    Serial.print("57");
+                 // 0x48,0x00 = NFB17 select5
+                 // irsend.sendNEC(0x48b700ff,32)
+                    delay(100);
                     break;
                 }
                
