137b0f81a7ee867de03c32dac137262c1bfb1ed0
Brendan Horan <brendanhoran@basstech.net>
2014-01-29 15:02:28 +0800

    Adding a local ir blower client, can be used from cron on the server.

---
+++ ir-blower-lclient.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+# Author  : Brendan Horan
+# License : BSD 3-Clause
+# Version : 0.0.1
+#
+# Connects to the local ir-blower server
+# on port 2000 and sends an option to the server.
+# For example you can use this from CRON to turn 
+# on a device via IR blower server.
+
+require 'socket'
+
+
+if ARGV.length < 1
+  puts "Require IR blower option" 
+end
+
+lblower = TCPSocket.new 'localhost', 2000
+
+lblower.puts ARGV.first
+lblower.close
