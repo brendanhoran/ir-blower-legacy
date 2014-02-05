931039c69ec7ffa3c2d87fe6a5ee553632b3798b
Peter Membrey <peter@membrey.hk>
2014-02-05 15:23:02 +0800

    Supports multiple connections - i.e. in info window and cron runs at the same time...

--- ir-blower-server.rb
+++ ir-blower-server.rb
@@ -133,59 +133,61 @@ end
 
   loop do
     Thread.start($server.accept) do |client|
-        while ircommand  = client.gets
-
-        command = ircommand.chomp
-   
-        case command 
   
-        when "d1b1"
-            d1b1()
+    while ircommand  = client.gets
 
-        when "d1b2"
-            d1b2()
-
-        when "d1b3" 
-            d1b3()
-
-        when "d1b4"
-            d1b4()
-
-        when "d1b5"
-            d1b5()
+    command = ircommand.chomp
    
-        when "d1b6"
-            d1b6()
+    case command 
+  
+    when "d1b1"
+      d1b1()
 
-        when "d2b1"
-            d2b1()
+    when "d1b2"
+      d1b2()
+
+    when "d1b3" 
+      d1b3()
+
+    when "d1b4"
+      d1b4()
+
+    when "d1b5"
+      d1b5()
+   
+    when "d1b6"
+      d1b6()
+
+    when "d2b1"
+      d2b1()
     
-        when "d2b2"
-            d2b2()
+    when "d2b2"
+      d2b2()
 
-        when "d2b3"
-            d2b3()
+    when "d2b3"
+      d2b3()
 
-        when "d2b4"
-            d2b4()
+    when "d2b4"
+      d2b4()
 
-        when "d2b5"
-            d2b5()
+    when "d2b5"
+      d2b5()
 
-        when "d2b6"
-            d2b6()
+    when "d2b6"
+      d2b6()
 
-        when "status"
-            status(client)
+    when "status"
+      status(client)
 
-        else 
-            Syslog.log(Syslog::LOG_ERR, "IR-BLower got invalid client option!")
-        end 
+    else 
+      Syslog.log(Syslog::LOG_ERR, "IR-BLower got invalid client option!")
+    end 
 
-    end
+
+end
+
+
+
 end
 
 end
-
-
-
